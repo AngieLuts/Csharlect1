33e42eef5a60f16f530f6578dd72126c9116f3db
AngieLuts <angiegk@yandex.ru>
2023-10-04 19:49:37 +0300

    masha

--- Example005/Program.cs
+++ Example005/Program.cs
@@ -1,2 +1,14 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿
+Console.WriteLine("Type in your name");
+string username = Console.ReadLine();
+
+if(username.ToLower() == "masha")
+{
+         Console.WriteLine("Hurray, here is Masha!");
+}
+else
+{
+        Console.Write("Hello,");
+        Console.WriteLine(username);
+}
+
